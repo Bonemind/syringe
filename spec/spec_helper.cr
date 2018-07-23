309611a5702c5654154fe8f03dc4204d82a64668
Subhi Dweik <subhime@gmail.com>
2018-07-23 15:47:33 +0200

    Added tests

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,69 @@
 require "spec"
 require "../src/syringe"
+
+# Injectables, both macro and wrapping variants
+class IncludeInjectable
+  Syringe.injectable
+end
+
+class WrapInjectable
+end
+Syringe.injectable(WrapInjectable)
+
+# Providers, A variant where a new instance is returned,
+# and a variant where a singleton is provided
+class TestInstance
+  @counter = 0
+
+  def increment
+    @counter = @counter + 1
+  end
+
+  def get_counter
+    return @counter
+  end
+end
+
+class SingletonTestInstance < TestInstance
+end
+
+class InstanceProvider
+  Syringe.provide(TestInstance)
+
+  def self.getInstance
+    return TestInstance.new
+  end
+end
+
+class SingletonProvider
+  Syringe.provide(SingletonTestInstance)
+  @@instance = SingletonTestInstance.new
+
+  def self.getInstance
+    return @@instance
+  end
+end
+
+# Classes that get their dependencies injected, one where we
+# include syringe, the other where the class is wrapped afterwards
+class TInclude
+  include Syringe
+  Syringe.injectable
+  def initialize(@i : IncludeInjectable)
+  end
+
+  def i
+    return @i
+  end
+end
+
+class TRegistered
+  def initialize(@i : WrapInjectable)
+  end
+
+  def i
+    return @i
+  end
+end
+Syringe.wrap(TRegistered)
+
--- spec/syringe_spec.cr
+++ spec/syringe_spec.cr
@@ -1,9 +1,77 @@
 require "./spec_helper"
 
-describe Syringe do
-  # TODO: Write tests
+class ChainedDependencies
+  def initialize(@i : IncludeInjectable, @t : TInclude)
+  end
 
-  it "works" do
-    false.should eq(true)
+  def i
+    return @i
+  end
+
+  def t
+    return @t
+  end
+end
+Syringe.wrap(ChainedDependencies)
+
+class ProvidedInstance
+  def initialize(@i : TestInstance)
+  end
+
+  def i
+    return @i
+  end
+end
+Syringe.wrap(ProvidedInstance)
+
+class SingletonProvidedInstance
+  def initialize(@i : SingletonTestInstance)
+  end
+
+  def i
+    return @i
+  end
+end
+Syringe.wrap(SingletonProvidedInstance)
+
+describe Syringe do
+  it "should be able to inject when included" do
+    t = TInclude.new
+    t.i.should_not be_nil
+  end
+
+  it "should be able to inject when wrapped" do
+    t = TRegistered.new
+    t.i.should_not be_nil
+  end
+
+  it "should be able to resolve multiples and chains" do
+    t = ChainedDependencies.new
+    t.i.should_not be_nil
+    t.t.should_not be_nil
+  end
+
+  it "should inject provided instances" do
+    t = ProvidedInstance.new
+    t.i.should_not be_nil
+    t.i.increment
+    t.i.increment
+
+    t.i.get_counter.should eq(2)
+
+    t = ProvidedInstance.new
+    t.i.get_counter.should eq(0)
+  end
+
+  it "should inject provided singletons" do
+    t = SingletonProvidedInstance.new
+    t.i.should_not be_nil
+    t.i.increment
+    t.i.increment
+
+    t.i.get_counter.should eq(2)
+
+    t = SingletonProvidedInstance.new
+    t.i.get_counter.should eq(2)
   end
 end
