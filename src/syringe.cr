28e337a3ea56baa26cf06a6278ccc708f4cd87c7
Tom Elliot <telliot16@gmail.com>
2019-10-19 23:57:13 -0400

    Added comments and did some house cleaning

--- src/syringe.cr
+++ src/syringe.cr
@@ -8,29 +8,41 @@ module Syringe
 	macro wrap(klass)
 		macro finished
 			\{% for ctor in {{klass}}.methods.select { |m| m.name == "initialize" } %}
-				 def {{klass}}.new
-				    \{% nodule = {{klass.id.split("::").reject { |k| k == klass.id.split("::").last}.join("::") }} %}
-					\{% module_classes = Object.all_subclasses.select { |k| nodule != "" && k.id.includes?(nodule) }.map { |k| k.id.split("::").last } %}
-					 new(
-						\{% for arg in ctor.args %}
-							\{% if arg.restriction.is_a?(Generic) && arg.restriction.name.resolve == Array %}
-								\{% array_type_classes = arg.restriction.type_vars.map { |v| v.resolve } %}
+				def {{klass}}.new
+				  \{% klass_module = {{klass.id.split("::").reject { |k| k == klass.id.split("::").last}.join("::") }} %}
+					# Find all classes within this module.
+					\{% module_classes = Object.all_subclasses.select { |k| klass_module != "" && k.id.includes?(klass_module) }.map { |k| k.id.split("::").last } %}
+					# Call the defined constructor with injectable classes as the arguments.
+					new(
+            # Generate a block for every type defined in the constructor.
+            \{% for arg in ctor.args %}
+              # If the arg is an array of generics, we want to inject all descendants of a class.
+              \{% if arg.restriction.is_a?(Generic) && arg.restriction.name.resolve == Array %}
+                # Get the array base type.
+                \{% array_type_classes = arg.restriction.type_vars.map { |v| v.resolve } %}
+                # Get the descendants of a base type.
 								\{% array_type_classes_descendants = Object.all_subclasses.select { |m| array_type_classes.any? {|k| m.ancestors.includes?(k) } } %}
 							 	\{{arg.name.id}}: [
-									\{% for injectable_class in array_type_classes_descendants %}
-										\{% if arg.restriction.is_a?(Generic) && arg.restriction.name.resolve == Array %}
-											\{% if module_classes.includes?(injectable_class) %}
-												Syringe.get\{{nodule.id.tr(":","_")}}__\{{injectable_class.id.tr(":","_")}},
-											\{% else %}
-												Syringe.get\{{injectable_class.id.tr(":","_")}},
-											\{% end %}
-										\{% end %}
-									\{% end %}
+                  # Get instances for all descendants, and define the in an array.
+                  \{% for injectable_class in array_type_classes_descendants %}
+                    # If injected class is defined in module of this class.
+                    \{% if module_classes.includes?(injectable_class) %}
+                      # Call the get injected class method with the module.
+                      Syringe.get\{{klass_module.id.tr(":","_")}}__\{{injectable_class.id.tr(":","_")}},
+                    \{% else %}
+                      # Call the get injected class method.
+                      Syringe.get\{{injectable_class.id.tr(":","_")}},
+                    \{% end %}
+                  \{% end %}
+                # All items in array have to be of this array class method.
 								] of \{{ array_type_classes.first }}
-							\{% else %}
-								\{% if module_classes.includes?(arg.restriction.id.stringify) %}
-									\{{arg.name.id}}: Syringe.get\{{nodule.id.tr(":","_")}}__\{{arg.restriction.id.tr(":","_")}},
-								\{% else %}
+              \{% else %}
+                # If injected class is defined in module of this class.
+                \{% if module_classes.includes?(arg.restriction.id.stringify) %}
+                  # Call the get injected class method with the module.
+									\{{arg.name.id}}: Syringe.get\{{klass_module.id.tr(":","_")}}__\{{arg.restriction.id.tr(":","_")}},
+                \{% else %}
+                  # Call the get injected class method.
 									\{{arg.name.id}}: Syringe.get\{{arg.restriction.id.tr(":","_")}},
 								\{% end %}
 							\{% end %}
